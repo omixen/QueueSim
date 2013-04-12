80c263ceb02394423badbb1fcd3c25b5adf6d8a3
Tommy Setiawan <omixen@gmail.com>
2013-04-12 00:21:57 -0400

    some parts of Dispatcher class, need Queue.getType() and Queue.getLength(), init Customer class, empty Message class

---
+++ queuesim/src/domain/Customer.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Customer {
+    private String id;
+    private String type;
+    private int arrivalTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
--- queuesim/src/domain/Dispatcher.java
+++ queuesim/src/domain/Dispatcher.java
@@ -1,7 +1,74 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  */
-public class Dispatcher {
+public class Dispatcher implements Runnable {
+    private float arrivalRate;
+    private long sleepTime;
+    private ArrayList<Queue> queues;
+
+    public float getArrivalRate() {
+        return arrivalRate;
+    }
+
+    public void setArrivalRate(float arrivalRate) {
+        this.arrivalRate = arrivalRate;
+    }
+
+    public float getSleepTim() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(long sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    public ArrayList<Queue> getQueues() {
+        return this.queues;
+    }
+
+    public void setQueues(ArrayList<Queue> queues) {
+        this.queues = queues;
+    }
+
+    public void addQueue(Queue queue) {
+        if(this.queues != null) {
+            this.queues.add(queue);
+        }
+    }
+
+    public Dispatcher() {
+        this.setArrivalRate(0);
+        this.setQueues(new ArrayList<Queue>());
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(this.sleepTime);
+
+        } catch(InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    public ArrayList<Customer> createCustomers() {
+        return null;
+    }
+
+    public Queue getQueue(String type) {
+        Queue temp = null;
+        Iterator<Queue> iter = this.queues.iterator();
+        while(iter.hasNext()) {
+            Queue q = iter.next();
+            if(q.getType() == type && (temp == null  || q.getLength()<temp.getLength())) {
+                temp = q;
+            }
+        }
+        return temp;
+    }
+
 }
