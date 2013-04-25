2c216a979ee8e493ba2ba4778ae5c2b088cebaa2
bentonjimmy <benton.jimmy@gmail.com>
2013-04-25 07:44:23 -0300

    Update ServiceStation.java
    Updated all of ServiceStation

--- queuesim/src/domain/ServiceStation.java
+++ queuesim/src/domain/ServiceStation.java
@@ -1,5 +1,6 @@
 package domain;
 import java.util.ArrayList;
+import java.util.Iterator;
 /**
  *
  * + Customer.Type[] customerTypes : may accept multiple customer types
@@ -12,57 +13,87 @@ import java.util.ArrayList;
  *
  */
 public class ServiceStation {
-	private ArrayList<String> customerTypes;  						// enum instead?
-	private Customer currentCustomer;
-	private ArrayList<Queue> accessibleQueues;
+	private ArrayList<CustomerType> customerTypes;  						// enum instead?
+	private Customer customer;
+	private ArrayList<Queue> queues;
+	private long sleepTime;
 	
-	
-//	public void getCustomer(Customer customer){
-//		
-//		this.currentCustomer = customer;
-//		
-//	}
-	
-	public void removeCustomer(Queue queue){
-		
-		
+	public ServiceStation()
+	{
+		customerTypes = new ArrayList<CustomerType>();
+		queues = new ArrayList<Queue>();
+		sleepTime = 0;
 	}
 	
-	public void addAccessibleQueue(Queue queue){
-		
-		this.accessibleQueues.add(queue);
-		
-	}
-	
-	public void remAccessibleQueue(Queue queue){
-		
-		// Whats the best way to add/remove objects in Java?
-		// this.accessibleQueues.remove(queue);
-		
-	}
-	
-	public void setCurrentCustomer(Customer customer){
-		
-		this.currentCustomer = customer;
-	}
-	
-	public Customer getCurrentCustomer() {
-		
-		return this.currentCustomer;
-		
+	public ArrayList<CustomerType> getTypes() {
+		return customerTypes;
 	}
 
-//	public void setCustomerTypes(ArrayList<> customerTypes) {
-//		
-//		this.customerTypes = customerTypes;
-//	}
-//	
-//	public getCustomerTypes() {
-//		
-//		return this.customerTypes;
-//	}
+	public void setTypes(ArrayList<CustomerType> types) {
+		this.customerTypes = types;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public ArrayList<Queue> getQueues() {
+		return queues;
+	}
+
+	public void setQueues(ArrayList<Queue> queues) {
+		this.queues = queues;
+	}
+
+	public float getSleepTime() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(long sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+    
+    public void getNextCustomer()
+    {
+    	long earliestArrival = -1;
+    	Iterator<Queue> queueIter = queues.iterator();
+    	while(queueIter.hasNext()) //Iterate through queues
+    	{
+    		Queue tempQueue = queueIter.next();
+    		Iterator<CustomerType> typeIter = customerTypes.iterator();
+    		while(typeIter.hasNext()) //Iterate through Service Station types
+    		{
+    			CustomerType tempType = typeIter.next();
+    			Customer c = tempQueue.dequeue(tempType); //get the next available customer in the queue
+    			if(c != null)
+    			{
+    				if(earliestArrival == -1 || c.getArrivalTime() < earliestArrival)//Check if c arrived earlier
+    				{
+    					customer = c;
+    				}
+    			}
+    		}
+    	}
+    }
 	
-	
-	
-	
+    public void service()
+    {
+    	//service the customer
+    	customer = null; //Service station is finished with the customer
+    }
+    
+    public void run() {
+        try {
+            Thread.sleep(this.sleepTime);
+            
+            Long time = System.currentTimeMillis();
+
+        } catch(InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
 }
